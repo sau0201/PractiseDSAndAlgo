fc98cda1601e1761073ae34e76c3cea2eab23d3b
sharma121 <saurabh.sharma3@non.keysight.com>
2019-12-22 13:27:22 -0800

    Ductch Flag Problem

---
+++ Sorting/DutchFlagProblem.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+class DutchFlagProblem {
+
+    public static void main(String[] args) {
+        char flag[] = {'R','G','B','R','G','B','R','G','B'};
+        sortColors(flag);
+        for (int i = 0; i < flag.length; i++) {
+            System.out.println(flag[i]);
+        }
+    }
+    public static void sortColors(char [] nums) {
+        quickSort(nums, 0, nums.length-1);
+    }
+    
+    static void quickSort(char [] nums , int lo, int hi){
+        if(lo>=hi)return;
+        char pivot='G';
+        int lt=lo;
+        int i=lo;
+        int gt=hi;
+        
+        while(i<=gt){
+            while(i<=gt && nums[i]>pivot){
+                swap(nums, i,lt);
+                i++;lt++;
+            }
+            while(i<=gt&&nums[gt]<pivot){
+                gt--;
+            }
+            while(i<=gt&&nums[i]<pivot){
+                swap(nums, i,gt);
+                gt--;
+            }
+            while(i<=gt && nums[i]==pivot){
+                i++;
+            }
+        }
+        
+    }
+    
+    static void swap(char [] nums, int first, int second){
+        if(first!=second){
+            char temp = nums[second];
+            nums[second] = nums[first];
+            nums[first]= temp;
+        }
+    }
+}
