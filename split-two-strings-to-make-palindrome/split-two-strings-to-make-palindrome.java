cab36f37e87e629188ad23880cd2fbd38eac3c4d
Saurabh Sharma <17959926+sau0201@users.noreply.github.com>
2021-05-31 19:35:53 -0700

    Time: 3 ms (99.57%), Space: 39.7 MB (59.79%) - LeetHub

---
+++ split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean checkPalindromeFormation(String a, String b) {
+        if(isPalindrome(a)|| isPalindrome(b)){
+            return true;
+        }
+        
+        if(checkPalindrome(a, b) || checkPalindrome(b, a)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    boolean isPalindrome(String str){
+        int i = 0;
+        int j = str.length()-1;
+        while(i<j){
+            if(str.charAt(i) == str.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    boolean checkPalindrome(String a, String b){
+        int i=0; int j= b.length()-1;
+        
+        if(a.charAt(i)!=b.charAt(j)) return false;
+        
+        while(i<j){
+            if(a.charAt(i)==b.charAt(j)){
+                i++;
+                j--;
+                continue;
+            }else{
+                String str = b.substring(i, j+1);
+                String str2 = a.substring(i, j+1);
+                if(isPalindrome(str) || isPalindrome(str2)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
