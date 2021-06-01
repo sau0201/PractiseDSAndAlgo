27ae6e07d19ffc7a70b21e2d587be83db79d70c8
Saurabh Sharma <17959926+sau0201@users.noreply.github.com>
2021-05-31 20:12:25 -0700

    Time: 6 ms (35.53%), Space: 57.3 MB (5.11%) - LeetHub

--- split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.java
+++ split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean checkPalindromeFormation(String a, String b) {
-        if(isPalindrome(a)|| isPalindrome(b)){
-            return true;
-        }
         
         if(checkPalindrome(a, b) || checkPalindrome(b, a)){
             return true;
         }
-        
+
         return false;
     }
     
-    boolean isPalindrome(String str){
-        int i = 0;
-        int j = str.length()-1;
+    boolean isPalindrome(String str, int i, int j){
         while(i<j){
             if(str.charAt(i) == str.charAt(j)){
                 i++;
@@ -28,20 +23,16 @@ class Solution {
     boolean checkPalindrome(String a, String b){
         int i=0; int j= b.length()-1;
         
-        if(a.charAt(i)!=b.charAt(j)) return false;
-        
         while(i<j){
             if(a.charAt(i)==b.charAt(j)){
                 i++;
                 j--;
                 continue;
             }else{
-                String str = b.substring(i, j+1);
-                if(isPalindrome(str)){
+                if(isPalindrome(a, i, j)){
                     return true;
                 }
-                String str2 = a.substring(i, j+1);
-                if(isPalindrome(str2)){
+                if(isPalindrome(b, i, j)){
                     return true;
                 }
                 return false;
