1f4c0cdfa444815ab7109adcf0f038c5c29a3b7f
Saurabh Sharma <17959926+sau0201@users.noreply.github.com>
2021-07-07 07:49:22 -0700

    Time: 0 ms (100.00%), Space: 36.9 MB (42.54%) - LeetHub

--- implement-queue-using-stacks/implement-queue-using-stacks.java
+++ implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,45 +1,33 @@
 class MyQueue {
     Stack<Integer> stack;
-    Stack<Integer> stack2;
     /** Initialize your data structure here. */
     public MyQueue() {
-        stack = new Stack<>();
-        stack2 = new Stack<>();
+        stack = new Stack();
     }
     
     /** Push element x to the back of queue. */
     public void push(int x) {
-        stack.push(x);
+        if(stack.isEmpty()) stack.push(x);
+        else{
+            int num = stack.pop();
+            push(x);
+            stack.push(num);
+        } 
     }
     
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        if(!stack2.isEmpty()){
-            return stack2.pop();
-        }else{
-            while(!stack.isEmpty()){
-                stack2.push(stack.pop());
-            }
-            return stack2.pop();
-        }
+        return stack.pop();
     }
     
     /** Get the front element. */
     public int peek() {
-        if(!stack2.isEmpty()){
-            return stack2.peek();
-        }else{
-            while(!stack.isEmpty()){
-                stack2.push(stack.pop());
-            }
-            return stack2.peek();
-        }
+        return stack.peek();
     }
     
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        if(stack.isEmpty() && stack2.isEmpty()) return true;
-        else return false;
+        return stack.isEmpty();
     }
 }
 
